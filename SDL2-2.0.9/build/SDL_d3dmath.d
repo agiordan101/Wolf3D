6037ea809a7f3eb31d440b0a50edd9ba4be0895a
Antoine <agiordan@z1r4p5.le-101.fr>
2019-02-17 06:25:08 +0100

    save

--- SDL2-2.0.9/build/SDL_d3dmath.d
+++
@@ -1,7 +0,0 @@
-build/SDL_d3dmath.lo: \
-  /Users/agiordan/Documents/Wolf3D/SDL2-2.0.9/src/render/SDL_d3dmath.c \
-  /Users/agiordan/Documents/Wolf3D/SDL2-2.0.9/src/render/../SDL_internal.h \
-  /Users/agiordan/Documents/Wolf3D/SDL2-2.0.9/src/render/../dynapi/SDL_dynapi.h \
-  /Users/agiordan/Documents/Wolf3D/SDL2-2.0.9/src/render/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/13 18:26:02 by gmonacho     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/17 06:09:01 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/17 06:10:31 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -63,7 +63,7 @@ int		main(int ac, char **av)
 		while (++i < win.map.width)
 			printf("%lf - ", win.calculs.dist[i]);
 		collision(&(win.player), win.map);
-		//open_window();
+		open_window();
 	}
 	else
 		ft_putendl("usage: ./Wolf3D <map>\n");
