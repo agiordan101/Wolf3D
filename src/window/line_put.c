354cd9c79d00f7fd9ad63e4aff35280e8c414cad
Antoine <agiordan@z1r3p2.le-101.fr>
2019-02-18 20:12:42 +0100

    Opti

--- src/window/line_put.c
+++ src/window/line_put.c
@@ -6,7 +6,7 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/21 18:05:06 by gmonacho     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/18 20:05:47 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/18 20:12:13 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,25 +18,23 @@ void			line_put(t_win *win, t_dot_2d p1, t_dot_2d p2)
 	int			i;
 	int			nb_point;
 	t_vector_2d	delta;
+	double		tmp;
+	t_dot_2d	pixel;
 
 	delta.x = p2.x - p1.x;
 	delta.y = p2.y - p1.y;
 	if (ft_abs(delta.x) > ft_abs(delta.y))
-	{
-		nb_point = ft_abs(delta.x);
-		delta.y /= ft_abs(delta.x);
-		delta.x /= ft_abs(delta.x);
-	}
+		tmp = delta.x;
 	else
-	{
-		nb_point = ft_abs(delta.y);
-		delta.x /= ft_abs(delta.y);
-		delta.y /= ft_abs(delta.y);
-	}
+		tmp = delta.y;
+	nb_point = ft_abs(tmp);
+	delta.x /= ft_abs(tmp);
+	delta.y /= ft_abs(tmp);
 	i = -1;
 	while (i++ < nb_point)
 	{
-		if ((p1.y + delta.y * i) < win->height && (p1.y + delta.y * i) >= 0 && (p1.x + delta.x * i) < win->width && (p1.x + delta.x * i) >= 0)
-			SDL_RenderDrawPoint(win->rend, p1.x + delta.x * i, p1.y + delta.y * i);
+		pixel = (t_dot_2d){.x = p1.x + delta.x * i, .y = p1.y + delta.y * i};
+		if (pixel.y < win->height && pixel.y >= 0 && pixel.x < win->width && pixel.x >= 0)
+			SDL_RenderDrawPoint(win->rend, pixel.x, pixel.y);
 	}
 }
