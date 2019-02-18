608d04531424aa0e9c1131f18b0fea1c776e233d
Antoine <agiordan@z1r3p2.le-101.fr>
2019-02-18 20:14:33 +0100

    Opti

--- src/window/line_put.c
+++ src/window/line_put.c
@@ -6,7 +6,7 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/11/21 18:05:06 by gmonacho     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/18 20:12:13 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/18 20:13:54 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -16,7 +16,7 @@
 void			line_put(t_win *win, t_dot_2d p1, t_dot_2d p2)
 {
 	int			i;
-	int			nb_point;
+	int			n_pixels;
 	t_vector_2d	delta;
 	double		tmp;
 	t_dot_2d	pixel;
@@ -27,11 +27,11 @@ void			line_put(t_win *win, t_dot_2d p1, t_dot_2d p2)
 		tmp = delta.x;
 	else
 		tmp = delta.y;
-	nb_point = ft_abs(tmp);
+	n_pixels = ft_abs(tmp);
 	delta.x /= ft_abs(tmp);
 	delta.y /= ft_abs(tmp);
 	i = -1;
-	while (i++ < nb_point)
+	while (i++ < n_pixels)
 	{
 		pixel = (t_dot_2d){.x = p1.x + delta.x * i, .y = p1.y + delta.y * i};
 		if (pixel.y < win->height && pixel.y >= 0 && pixel.x < win->width && pixel.x >= 0)
