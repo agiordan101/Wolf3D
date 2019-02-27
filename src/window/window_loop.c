6b55564a189ed3ae165bd5ad792bc326d8302b3c
Antoine <agiordan@z1r3p4.le-101.fr>
2019-02-27 20:14:09 +0100

    + -

--- src/window/window_loop.c
+++ src/window/window_loop.c
@@ -3,10 +3,10 @@
 /*                                                              /             */
 /*   window_loop.c                                    .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: gmonacho <gmonacho@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/18 08:56:27 by gmonacho     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/27 20:04:59 by gmonacho    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/27 20:13:36 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -27,14 +27,6 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 	double	pas;
 
 	pas = 0.1;
-	/*if (state[SDL_SCANCODE_W])
-		win->player.pos.y -= win->player.pos.y - pas <= 0 ? 0 : pas;
-	if (state[SDL_SCANCODE_S])
-		win->player.pos.y += win->player.pos.y + pas > win->map.len_y ? 0 : pas;
-	if (state[SDL_SCANCODE_A])
-		win->player.pos.x -= win->player.pos.x - pas <= 0 ? 0 : pas;
-	if (state[SDL_SCANCODE_D])
-		win->player.pos.x += win->player.pos.x + pas > win->map.len_x[(int)win->player.pos.y] ? 0 : pas;*/
 	if (state[SDL_SCANCODE_W])
 	{
 		win->player.pos.y -= (win->player.pos.y - sin(win->player.dir) * pas < 0) ||\
@@ -72,29 +64,19 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 							0 : cos(win->player.dir - PI / 2) * pas;
 	}
 	if (state[SDL_SCANCODE_RIGHT])
-	{
-		printf("RIGHT\n");
 		win->player.dir -= 0.1;
-	}
 	if (state[SDL_SCANCODE_LEFT])
-	{
-		printf("LEFT\n");
 		win->player.dir += 0.1;
-	}
 }
 
 static int	keyboard_event(t_win *win, SDL_Event event)
 {
 	if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
 		return (0);
-	if (event.key.keysym.sym == SDLK_x)
-		win->player.dir += 0.1;
-	if (event.key.keysym.sym == SDLK_c)
-		win->player.dir -= 0.1;
-	/*if (event.key.keysym.sym == SDLK_'+')
-		win->player.fov += 0.1;
-	if (event.key.keysym.sym == SDLK_'-')
-		win->player.fov -= 0.1;*/
+	if (event.key.keysym.sym == SDLK_MINUS)
+		win->player.fov -= 10;
+	if (event.key.keysym.sym == SDLK_PLUS)
+		win->player.fov += 10;
 	return (1);
 }
 
