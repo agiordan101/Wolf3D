e5733ef831e078dcc4353c9d2f9a401c1d127b8f
Antoine <agiordan@z1r3p4.le-101.fr>
2019-02-27 20:41:47 +0100

    fov et collisions

--- src/window/window_loop.c
+++ src/window/window_loop.c
@@ -6,7 +6,7 @@
 /*   By: agiordan <agiordan@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/02/18 08:56:27 by gmonacho     #+#   ##    ##    #+#       */
-/*   Updated: 2019/02/27 20:13:36 by agiordan    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/02/27 20:39:48 by agiordan    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -32,8 +32,9 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 		win->player.pos.y -= (win->player.pos.y - sin(win->player.dir) * pas < 0) ||\
 					(win->player.pos.y - sin(win->player.dir) * pas > win->map.len_y) ?\
 					0 : sin(win->player.dir) * pas;
-		win->player.pos.x += win->player.pos.x + cos(win->player.dir) * pas\
-							> win->map.len_x[(int)win->player.pos.y] ?\
+		win->player.pos.x += (win->player.pos.x + cos(win->player.dir) * pas\
+							> win->map.len_x[(int)win->player.pos.y]) ||\
+							(win->player.pos.x + cos(win->player.dir) * pas < 0) ?\
 							0 : cos(win->player.dir) * pas;
 	}
 	if (state[SDL_SCANCODE_S])
@@ -41,8 +42,9 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 		win->player.pos.y -= (win->player.pos.y - sin(win->player.dir + PI) * pas < 0) ||\
 					(win->player.pos.y - sin(win->player.dir + PI) * pas > win->map.len_y) ?\
 					0 : sin(win->player.dir + PI) * pas;
-		win->player.pos.x += win->player.pos.x + cos(win->player.dir + PI) * pas\
-							> win->map.len_x[(int)win->player.pos.y] ?\
+		win->player.pos.x += (win->player.pos.x + cos(win->player.dir + PI) * pas\
+							> win->map.len_x[(int)win->player.pos.y]) ||\
+							(win->player.pos.x + cos(win->player.dir + PI) * pas < 0) ?\
 							0 : cos(win->player.dir + PI) * pas;
 	}
 	if (state[SDL_SCANCODE_A])
@@ -51,7 +53,8 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 					(win->player.pos.y - sin(win->player.dir + PI / 2) * pas > win->map.len_y) ?\
 					0 : sin(win->player.dir + PI / 2) * pas;
 		win->player.pos.x += win->player.pos.x + cos(win->player.dir + PI / 2) * pas\
-							> win->map.len_x[(int)win->player.pos.y] ?\
+							> win->map.len_x[(int)win->player.pos.y] ||\
+							(win->player.pos.x + cos(win->player.dir + PI / 2) * pas < 0) ?\
 							0 : cos(win->player.dir + PI / 2) * pas;
 	}
 	if (state[SDL_SCANCODE_D])
@@ -60,7 +63,8 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 					(win->player.pos.y - sin(win->player.dir - PI / 2) * pas > win->map.len_y) ?\
 					0 : sin(win->player.dir - PI / 2) * pas;
 		win->player.pos.x += win->player.pos.x + cos(win->player.dir - PI / 2) * pas\
-							> win->map.len_x[(int)win->player.pos.y] ?\
+							> win->map.len_x[(int)win->player.pos.y] ||\
+							(win->player.pos.x + cos(win->player.dir - PI / 2) * pas < 0) ?\
 							0 : cos(win->player.dir - PI / 2) * pas;
 	}
 	if (state[SDL_SCANCODE_RIGHT])
@@ -71,12 +75,15 @@ static void keyboard_state(t_win *win, const Uint8 *state)
 
 static int	keyboard_event(t_win *win, SDL_Event event)
 {
+	double	dfov;
+
+	dfov = 0.1;
 	if (event.type == SDL_QUIT || event.key.keysym.sym == SDLK_ESCAPE)
 		return (0);
-	if (event.key.keysym.sym == SDLK_MINUS)
-		win->player.fov -= 10;
-	if (event.key.keysym.sym == SDLK_PLUS)
-		win->player.fov += 10;
+	if (event.key.keysym.sym == SDLK_KP_MINUS)
+		win->player.fov -= win->player.fov - dfov < 0 ? 0 : dfov;
+	if (event.key.keysym.sym == SDLK_KP_PLUS)
+		win->player.fov += win->player.fov + dfov > 2 * PI ? 0 : dfov;
 	return (1);
 }
 
@@ -99,6 +106,7 @@ int window_loop(t_win *win)
 			if (!keyboard_event(win, event))
 				loop = 0;
 		}
+		collision(&(win->player), win->map);
 		refresh_window(win);
 	}
 	return (1);
